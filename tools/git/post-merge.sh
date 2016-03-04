49f04882b1c2ff77ae65d200346620171794cd5a
hmrocket <hmrocket@gmail.com>
2016-03-03 19:15:38 -0500

    git hooks examples

---
+++ tools/git/post-merge.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+#When you run git pull, git actually does a fetch followed by a merge. 
+#This means you can use the post-merge hook to execute a script when a pull is completed.
+# 1- Check if the master was updated
+# 2- Increment Android Build version code (auto)
+# 3 - Increment Android version code (ask user to enter new version name)
+# 4- add and commit Android manifest or app.gradle
+# 5- push force to the master
+
+echo "post-merge hooks is running:"
+#1
+production_branch='master'
+MANIFEST="build.grade"
+versionName=0
+versionCode=0
+
+current_branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
+
+if [[ $current_branch = $protected_branch ]]; then
+	echo "Continuous Integration: try ++ version name and ++ build version"
+    updateVersionCode()
+    updateVersionName()
+	git add $MANIFEST
+	git commit -m "Bump $versionName"
+	git tag $versionName -m "Build version $versionCode"
+#	git push --force
+#	git push --tags
+fi
+exit 0
+
+#2
+function updateVersionCode {
+
+if [ -f $MANIFEST ]
+then
+    LINE=$(grep -o ${MANIFEST} -e 'versionCode [0-9]*');
+	declare -a LINE;
+	LINE=(`echo $LINE | tr "\"" " "`);
+	VERSION=(${LINE[1]});
+	INCREMENTED=$(($VERSION+1))
+    sed "s/versionCode ${INCREMENTED}/" $MANIFEST > $MANIFEST.tmp && mv $MANIFEST.tmp $MANIFEST
+    git add $MANIFEST
+    echo "Updated Build Number To ${INCREMENTED} In ${MANIFEST}!";
+	versionCode=VERSION
+fi
+}
+#3
+function updateVersionName {
+
+if [ -f $MANIFEST ]
+then
+    LINE=$(grep -o ${MANIFEST} -e 'versionName "^[0-9]+(\.\d+)?"');
+	declare -a LINE;
+	LINE=(`echo $LINE | tr "\"" " "`);
+	VERSION=(${LINE[1]});
+	INCREMENTED=$(($VERSION+1))
+	echo "Current Version Name ${VERSION} In ${MANIFEST}!";
+	echo "Please updated version Name and press [ENTER]:"
+	read versionName
+	
+	while [[ ! $versionName =~ ^[0-9]+(\.\d+)? ]]; do
+		echo "invalid Version Name entry! again:"
+		read versionName
+	done
+	sed "s/versionName \"${INCREMENTED}\"/" $MANIFEST > $MANIFEST.tmp && mv $MANIFEST.tmp $MANIFEST
+	git add $MANIFEST
+	echo "Version Name update to ${INCREMENTED} In ${MANIFEST}!";
+	versionName=INCREMENTED
+fi
+
+}
